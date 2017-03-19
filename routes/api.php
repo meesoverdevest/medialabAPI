55de29940f9221ff2f610a3b1468d954436c2840
biertjesman <mees@mefolio.nl>
2017-03-19 19:55:37 +0000

    Add additional controllers

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('protectedTest','API\RegisterController@protectedTest');
+Route::group(['middleware' => 'auth:api'], function() {
+	Route::get('protectedTest','API\RegisterController@protectedTest');
+
+});
+

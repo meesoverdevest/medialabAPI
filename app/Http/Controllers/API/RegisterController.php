fb1566b49a8451df24d347f096cf0d25c98a60cb
biertjesman <mees@mefolio.nl>
2017-03-19 12:43:07 +0000

    Update test API methods

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -9,10 +9,21 @@ use App\User;
 class RegisterController extends Controller
 {
   	public function register(Request $request) {
-  		return $request;
+  		// User::create([
+	   //      'name' => $data['name'],
+	   //      'email' => $data['email'],
+	   //      'password' => bcrypt($data['password']),
+	   //      'api_token' => str_random(60),
+	   //  ]);
+
+  		$name = $request->get('name');
+
+  		return response()->json(compact('name'));
   	}
 
   	public function test(Request $request) {
-  		return "test";
+  		$name = "adfsd";
+
+  		return response()->json(['name' => $name], 200);
   	}
 }

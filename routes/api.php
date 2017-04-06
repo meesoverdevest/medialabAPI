f92c752b05a47234bb1bf4096761ff03e24a4c64
biertjesman <mees@mefolio.nl>
2017-04-06 09:03:22 +0000

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'auth:api'], function() {
 	Route::get('protectedTest','API\RegisterController@protectedTest');
 	Route::resource('adjustments', 'API\AdjustmentsController', ['as' => 'api']);
+	Route::resource('reactions', 'API\ReactionController', ['as' => 'api']);
 
 });
 
-Route::resource('reactions', 'API\ReactionController', ['as' => 'api']);
 
 
 Route::post('register', ['as' => 'api.register', 'uses' => 'API\RegisterController@register']);
